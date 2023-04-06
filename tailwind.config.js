be08104aca66ea11a25f12bf55cbb4880623037d
YanivWein24 <yanivwein22@gmail.com>
2023-04-06 19:12:32 +0300

    install dependencies, setup es-lint & tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        "fade-in": "fadeIn 0.25s ease-in-out",
+      },
+    },
+  },
+  plugins: [],
+}
+
