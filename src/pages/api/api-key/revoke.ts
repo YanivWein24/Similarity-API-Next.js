8425ba28bea4cdde3142fd362617ec2dff75676a
YanivWein24 <yanivwein22@gmail.com>
2023-04-16 15:36:25 +0300

    feat: revoking api keys
    * create new handler for revoking api keys
    * create fetch function inside "helpers" directory to call the revoke handler

---
+++ src/pages/api/api-key/revoke.ts
@@ -0,0 +1,55 @@
+import withMethods from "@/lib/api-middleware/withMethods";
+import { authOptions } from "@/lib/auth";
+import { RevokeApiData } from "@/types/api";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { db } from "../../../../prisma/db";
+import { z } from "zod";
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<RevokeApiData>
+) => {
+  try {
+    const user = await getServerSession(req, res, authOptions).then(
+      (res) => res?.user
+    );
+
+    if (!user)
+      return res.status(401).json({
+        error: "Unauthorized to perform this action",
+        success: false,
+      });
+
+    const validApiKey = await db.apiKey.findFirst({
+      where: { userId: user.id, enabled: true },
+    });
+
+    if (!validApiKey) {
+      return res.status(500).json({
+        error: "This API key could not be revoked.",
+        success: false,
+      });
+    }
+
+    // invalidate the api key
+    await db.apiKey.update({
+      where: {
+        id: validApiKey.id,
+      },
+      data: { enabled: false },
+    });
+
+    return res.status(200).json({ error: null, success: true });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({ error: error.issues, success: false });
+    }
+
+    return res
+      .status(500)
+      .json({ error: "Internal server error", success: false });
+  }
+};
+
+export default withMethods(["POST"], handler);
