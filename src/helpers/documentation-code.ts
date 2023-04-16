53636ac42d45d5d23965d8abed68a7b661b2d518
YanivWein24 <yanivwein22@gmail.com>
2023-04-16 18:44:14 +0300

    update api endpoints in documentation-code.ts

--- src/helpers/documentation-code.ts
+++ src/helpers/documentation-code.ts
@@ -2,7 +2,7 @@ export const nodejs = `const axios = require("axios");
 
 const options = {
     method: "POST",
-    url: "https://similarityapi.com/api/v1/similarity",
+    url: "https://similarity-api-next.vercel.app/api/v1/similarity",
     params: {
       text1: "First text",
       text2: "Second text"
@@ -20,7 +20,7 @@ axios.request(options).then(function (response) {
 
 export const python = `import requests
 
-url = "https://similarityapi.com/api/v1/similarity"
+url = "https://similarity-api-next.vercel.app/api/v1/similarity"
 api_key = "YOUR_API_KEY"
 text1 = "First text"
 text2 = "Second text"
@@ -44,7 +44,7 @@ else:
 
 export const bash = `curl -X POST \
 
-"https://similarityapi.com/api/v1/similarity" \
+"https://similarity-api-next.vercel.app/api/v1/similarity" \
 
 -H "Authorization: YOUR_API_KEY" \
 
